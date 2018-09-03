141c3d9b2a6147531e4c421a34d51192a06d81ed
Gonzalo Matheu <gmatheu@ascentio.com.ar>
2018-09-03 12:24:13 -0300

    Parameterizing image tag on make file

--- inc/docker.mk
+++ inc/docker.mk
@@ -7,12 +7,12 @@ pre-build-image:
 pull-images:
 
 build-image: pull-images pre-build-image
-	docker build ${BUILD_OPTS} --build-arg https_proxy=${HTTPS_PROXY} --build-arg http_proxy=${HTTP_PROXY} --tag ${IMAGE}:latest .
+	docker build ${BUILD_OPTS} --build-arg https_proxy=${HTTPS_PROXY} --build-arg http_proxy=${HTTP_PROXY} --tag ${IMAGE}:${IMAGE_TAG} .
 build-image-with-local-cache: pull-images pre-build-image
 	$(MAKE) build-image HTTP_PROXY=http://172.17.0.1:3128 HTTPS_PROXY=http://172.17.0.1:3128
+IMAGE_TAG=latest
 push-image: build-image
-	docker tag ${IMAGE}:latest ${INTERNAL_REGISTRY_URL}/${IMAGE}:latest
-	docker push ${INTERNAL_REGISTRY_URL}/${IMAGE}:latest
+	docker push ${IMAGE}:${IMAGE_TAG}
 
 lint-image:
 	@echo 'More about Hadolint rules: https://github.com/hadolint/hadolint#rules'
